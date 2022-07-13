16a75753dbf74a5804c856ea75518012d6a002b3
John Christensen <Johnedchristensen@gmail.com>
2022-07-13 10:20:09 -0500

    Path Change

--- Southern_Nebula.js
+++ Southern_Nebula.js
@@ -1,11 +1,11 @@
 var sources = [
     {
         type: 'image',
-        url: '/img/hubble/southern_nebula.png'
+        url: 'img/hubble/southern_nebula.png'
     },
     {
         type: 'image',
-        url: '/img/webb/southern_nebula.png'
+        url: 'img/webb/southern_nebula.png'
     }
 ];
 createView(sources);
